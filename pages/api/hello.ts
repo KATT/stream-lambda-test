beb46c97266e130885a7100b015ddc8af23594ae
KATT <alexander@n1s.se>
2024-05-15 12:54:32 +0200

    wat

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,30 @@ type Data = {
   name: string;
 };
 
-export default function handler(
+const waitMs = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.setHeader("Transfer-Encoding", "chunked");
+
+  console.log(
+    "flush exists?",
+    // @ts-expect-error this exists locally at least
+    !!res?.flush
+  );
+  for (let i = 0; i < 1_000; i++) {
+    res.chunkedEncoding = true;
+    res.write(`data: ${i}\n\n`);
+
+    // @ts-expect-error this exists locally at least
+    res?.flush();
+
+    await waitMs(3);
+  }
+  res.end();
 }
